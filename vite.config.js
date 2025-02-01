db22368a844eae8d3f48fdd4055e3c6cb341ee24
WiGram <william.gram@outlook.com>
2025-02-01 15:11:13 +0100

    Added another blog post

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
         main: 'index.html',
         'projects/regulatory-reporting': 'projects/regulatory-reporting/index.html',
         'projects/performance-pay': 'projects/performance-pay/index.html',
-        'blog_posts/api-audit-logging': 'blog_posts/api-audit-logging/index.html'
+        'blog_posts/api-audit-logging': 'blog_posts/api-audit-logging/index.html',
+        'blog_posts/netlify-supabase-setup': 'blog_posts/netlify-supabase-setup/index.html'
       }
     },
     outDir: 'dist',
