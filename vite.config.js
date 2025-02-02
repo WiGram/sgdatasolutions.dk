70ca97936c54b8970d445d00402b24ec10531773
WiGram <william.gram@outlook.com>
2025-02-02 01:23:42 +0100

    Added data platform project

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         main: 'index.html',
         'projects/regulatory-reporting': 'projects/regulatory-reporting/index.html',
         'projects/performance-pay': 'projects/performance-pay/index.html',
+        'projects/data-platform': 'projects/data-platform/index.html',
         'blog_posts/api-audit-logging': 'blog_posts/api-audit-logging/index.html',
         'blog_posts/netlify-supabase-setup': 'blog_posts/netlify-supabase-setup/index.html'
       }
